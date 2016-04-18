e07afaf4d4f272cdd4c94d8f1b29ac08acdb98ab
Alex Ung <alexung.ucsb@gmail.com>
2016-04-18 17:17:37 +0900

    Get article page to work

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     #binding.pry
-    #puts @category
+    @articles = @category.articles
   end
 end
